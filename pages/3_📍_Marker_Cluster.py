ae4a5cf75b9dd52b05dab2e85341ce0bdea95967
vmiller-geo <electricradium@proton.me>
2024-09-26 09:27:21 -0400

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -19,16 +19,13 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
-
-        m.add_geojson(regions, layer_name="US Regions")
+        cases = "cleaned_ccdb_st.csv"
+                
         m.add_points_from_xy(
-            cities,
-            x="longitude",
-            y="latitude",
-            color_column="region",
-            icon_names=["gear", "map", "leaf", "globe"],
+            cases,
+            x="Lon",
+            y="Lat",
+            color_column="type",
             spin=True,
             add_legend=True,
         )
