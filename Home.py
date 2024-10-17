afe746d7b52e527d7f6e958e2420c1516d76b593
vmiller-geo <electricradium@proton.me>
2024-10-17 18:11:50 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,22 +19,18 @@ st.title("Streamlit for Geospatial Applications")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    This app is an interactive web app created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). 
     """
 )
 
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-
+* For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
 """
 
 st.markdown(markdown)
 
 m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
+m.add_basemap("Esri.NatGeoWorldMap")
 m.to_streamlit(height=500)
